ac1c54096e8ccf7301b825a4d634f18121711132
Green Kobold <renato.fabbri@gmail.com>
2013-02-28 18:34:36 -0300

    feita versao em que os compassos sao achados

--- ana2.py
+++ ana2.py
@@ -57,9 +57,11 @@ intervalos_scar = [[9, -2, -2, -5],
 # vamos armazenar em encontrados as quantidades de motivos encontrados em
 # beethoven
 encontrados = []
+onde=[]
 # para cada motivo do scarlatti...
 for inte in intervalos_scar:
     conta_encontrados = 0
+    onde.append([])
     # vamos procurar em todos os trechos possíveis das notas do beethoven
     for i in xrange(len(intervalos_bee)):
         trecho = intervalos_bee[i:len(inte)+i]
@@ -67,7 +69,13 @@ for inte in intervalos_scar:
             print "Procurando %s em %s: %s" % (inte, intervalos_bee, trecho)
             # quando os intervalos baterem, somamos 1 aos encontrados
             if list((n.array(inte)>0)*2-1) == list((n.array(trecho)>0)*2-1):
+            #if inte == trecho:
                 conta_encontrados = conta_encontrados + 1
+                onde[-1].append(i)
+            if list((n.array(inte)>0)*2-1) == -1*list((n.array(trecho)>0)*2-1):
+            #if inte == [-t for t in trecho]:
+                conta_encontrados = conta_encontrados + 1
+                onde[-1].append(i)
     encontrados.append(conta_encontrados)
 
 # portanto, encontrados é uma lista com a quantidade de cada motivo encontrado
@@ -76,3 +84,49 @@ for inte in intervalos_scar:
 for i in xrange(len(intervalos_scar)):
     print 'Encontrados %s motivos iguais a %s.' % (encontrados[i],
                                                    intervalos_scar[i])
+
+# percorremos cada compasso analisando as notas
+comp = []
+for compasso in compassos.getElementsByClass(m.stream.Measure):
+    print 'Compasso:', compasso.measureNumber
+    notas = []
+    # percorremos cada nota do compasso
+    for nota in compasso.notes:
+        # consideramos apenas notas
+        if type(nota) == m.note.Note:
+            # guardamos o valor MIDI da nota
+            notas.append(nota.midi)
+            print nota, nota.midi
+    # guardamos todas as notas desse compasso em uma lista
+    comp.append(notas)
+
+compassos_incidencias=[]
+
+for o in onde:
+    compassos_incidencias.append([])
+    ii=0
+    compasso=0
+    for i in o: # para cada incidencia do motivo
+        n_notas=0
+        while ii<i: #achando a nota
+           c=[x[0] for x in groupby(comp[compasso])]
+           ii+=len(c)
+           if compasso > 0:
+               try:
+                   if c[0] ==comp[compasso-1][-1]:
+                       ii-=1
+               except:
+                   pass
+           compasso+=1
+        compassos_incidencias[-1].append(compasso)
+
+            
+            
+
+
+
+
+
+
+
+
