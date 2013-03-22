1a80c3ea8fd920ffdf9dce33cbeb52af77a1c9b4
Vilson Vieira <vilson@void.cc>
2013-03-22 19:52:41 -0300

    ana4.py ok, but we have to validate

--- ana4.py
+++ ana4.py
@@ -3,11 +3,24 @@
 import music21 as m, numpy as n
 from itertools import groupby
 
-# importamos Piano Sonata No. 1 in F  minor, Op. 2, No. 1 [1793-5]
+# parametros
+MAX_TOLERANCIA = 5
+ARQ_SCARLATTI = 'motscar.txt'
+
+# teste
+#intervalos_scar = [[1,1], [1,2]]
+#comps = [[], [60,62,64], [], [60,62,66, 60, 61], [60], [], [60,62], [60,60,62]]
+
+# lemos os motivos do arquivo de conf
+f = open(ARQ_SCARLATTI, 'r')
+ln = f.readlines()
+intervalos_scar = [[int(x) for x in l[1:-2].split(',')] for l in ln]
+
+# importamos a sonata
 opus = m.converter.parse('corpus/sonata27-2.krn')
 
 # vozes
-print [voz.id for voz in opus.parts]
+#print [voz.id for voz in opus.parts]
 # spine_2 => G
 # spine_0 => F
 
@@ -20,7 +33,7 @@ compassos = voz.measures(1, 1000)
 # percorremos cada compasso analisando as notas
 comps = []
 for compasso in compassos.getElementsByClass(m.stream.Measure)[:20]:
-    print 'Compasso:', compasso.measureNumber
+    #print 'Compasso:', compasso.measureNumber
     notas = []
     # percorremos cada nota do compasso
     for nota in compasso.notes:
@@ -28,7 +41,7 @@ for compasso in compassos.getElementsByClass(m.stream.Measure)[:20]:
         if type(nota) == m.note.Note:
             # guardamos o valor MIDI da nota
             notas.append(nota.midi)
-            print nota, nota.midi
+            #print nota, nota.midi
     # guardamos todas as notas desse compasso em uma lista
     comps.append(notas)
 
@@ -47,52 +60,90 @@ for i in xrange(len(comps)-1):
                 break
             j += 1
 
-notas = sum(comps, [])
+# cria lista de intervalos considerando compassos
+inters = []
+ultimo = None
+for i in xrange(len(comps)):
+    if len(comps[i]) == 0:
+        inters.append([])
+    else:
+        if ultimo == None:
+            l = []
+            for j in xrange(len(comps[i])-1):
+                l.append(comps[i][j+1] - comps[i][j])
+                ultimo = comps[i][j+1]
+            inters.append(l)
+        else:
+            l = [comps[i][0] - ultimo]
+            for j in xrange(len(comps[i])-1):
+                l.append(comps[i][j+1] - comps[i][j])
+                ultimo = comps[i][j+1]
+            if len(comps[i]) == 1:
+                ultimo = comps[i][0]
+            inters.append(l)
 
-inters = [x-y for x,y in zip(notas, notas[1:])]
+linguicao = sum(inters, [])
 
-# [comp_ini, nota_ini, comp_fim, nota_fim, inter_comp-inter_scar]
-
-intervalos_scar = [[9, -2, -2, -5],
-                   [2, 2, -2, -2, 2, 2, -2, -2],
-                   [4, 3, 5, 7, -3, -4],
-                   [1, 4, -7]]
-
-MAX_TOLERANCIA = 12
-
-CC = []
+# percorremos lista de intervalos e coletamos as incidências para cada motivo
+# intervalar de scarlatti
+C = []
+ci = 0
 for inter_scar in intervalos_scar:
-    C = []
-    for tol in xrange(MAX_TOLERANCIA):
+    C_inter = []
+    for tol in range(MAX_TOLERANCIA):
+        C_tol = []
         for i in xrange(len(inters)):
-            inter = inters[i:i+len(inter_scar)]
-            if len(inter) < len(inter_scar):
-                break
-            inci = sum([inter[j]-tol < inter_scar[j] < inter[j]+tol for j in range(len(inter_scar))])
-            if inci == len(inter_scar):
-                C.append([0, 0, 0, 0, [inter[k]-inter_scar[k] for k in xrange(len(inter))]])
-    CC.append(C)
+            inter = inters[i]
+            for j in xrange(len(inter)):
+                trecho = inter[j:j+len(inter_scar)]
+                c = None
+                if len(trecho) < len(inter_scar): # preciso de notas
+                    completado = trecho[:]
+                    k = 1
+                    falta = None
+                    # procuro notas nos proximos k compassos, ateh me fartar
+                    while ((len(inter_scar) - len(completado)) > 0) and ((i+k) < len(inters)):
+                        falta = len(inter_scar) - len(completado)
+                        if (i+k) < len(inters):
+                            if len(inters[i+k]):
+                                completado.extend(inters[i+k][:falta])
+                            k += 1
+                    if len(completado) == len(inter_scar):
+                        c = [i, j, i+k-1, falta-1, completado, [completado[x]-inter_scar[x] for x in xrange(len(inter_scar))]]
+                else: # nao preciso procurar mais notas, jah estah ok
+                    c = [i, j, i, j+len(inter_scar)-1, trecho, [trecho[x]-inter_scar[x] for x in xrange(len(inter_scar))]]
+                    
+                if c != None:
+                    #print c
+                    inci = [c[4][x]-tol <= inter_scar[x] <= c[4][x]+tol for x in xrange(len(inter_scar))]
+                    if sum(inci) == len(inter_scar):
+                        C_tol.append(c)
+        C_inter.append(C_tol)
+    C.append(C_inter)
 
-# CC = []
-# for inter_scar in intervalos_scar:
-#     C = []
-#     for tol in xrange(MAX_TOLERANCIA):
-#         for i in xrange(len(comps)):
-#             comp = comps[i]
-#             inter_comp = [x-y for x,y in zip(comp, comp[1:])]
-#             if len(inter_scar) > len(inter_comp):
-#                 # faltaram notas nesse compasso, precisamos pegar de outros
-#                 necessarios = len(inter_scar) - len(inter_comp)
-#                 qtds = []
-#                 for k in xrange(i+1, len(comps)):
-#                     necessarios = necessarios - len(comps[k])
-#                     qtds.append(comps[k])
-#                     if necessarios <= 0:
-#                         break
-#                 # coletamos as quantidades de notas faltantes
-#                 notas 
+# sobre C...
+#
+# cada elemento em C possui uma lista com as tolerâncias
+# cada uma dessas listas contém uma lista de incidências
+# cada incidência tem o seguinte formato:
+#
+#   [compasso ini,
+#    nota ini,
+#    compasso fim,
+#    nota fim,
+#    motivo encontrado,
+#    motivo encontrado - motivo alvo]
 
-#             inci = sum([inter_comp[j]-tol < inter_scar[j] < inter_comp[j]+tol for j in range(len(inter_scar))])
-#             if inci == len(inter_scar):
-#                 C.append([comp_ini, nota_ini, comp_fim, nota_fim, inter_comp-inter_scar])
-
+# gerando resultado/saida
+for i in xrange(len(C)):
+    j = 0
+    for inci in C[i]:
+        print '\n\nPara motivo %s e tolerância +-%s, encontradas %s incidências.\n\n' % (intervalos_scar[i], j+1, len(inci))
+        if inci != []:
+            print 'C.ini\tN ini\tC.fim\tN.fim\tM.encontrado'
+            print '-' * 80
+            for incii in inci:
+                print '%s\t%s\t%s\t%s\t%s' % (incii[0]+1, incii[1]+1, incii[2]+1, incii[3]+1, incii[4])
+        j += 1
+    print '\n'
+    print '*' * 80
